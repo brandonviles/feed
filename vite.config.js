31a47a66bc5dd28cc32baef315aaa12b3f5b14af
Brandon <brandonviles86@gmail.com>
2024-06-26 18:00:36 -0500

    dist

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/test/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/test/' : '/feed/',
   build: {
     outDir: 'docs'
   },
